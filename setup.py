87c7e85620b1ab52683dced0483fe6440e1df5b9
unknown <haopronk123@gmail.com>
2024-05-13 20:05:54 +0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author= 'Hoang Hao',
+    author_email= 'haopronk123@gmail.com',
+    packages= find_packages(),
+    install_requires = []del index.lock
+
+)
